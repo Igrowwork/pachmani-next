a74da12ddbba3a92599fc24cae02db5df8781164
Aniket <aniketkalawat88@gmail.com>
2024-04-04 17:45:29 +0530

    footer-navbar-changes

--- src/components/footer/page.tsx
+++ src/components/footer/page.tsx
@@ -2,47 +2,48 @@ import { forum } from "@/app/font";
 import { Icons } from "@/app/icons";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 export default function Footer() {
   return (
     <div className=" mt-10 relative w-full bg-[#EEEDEC] text-[#212B36] md:p-10 p-6">
-        <Image src='/Assests/Images/HomeImage/21.png' alt="No Preview" fill className="object-contain max-sm:hidden object-left-bottom"/>
-        <Image src='/Assests/Images/HomeImage/11.png' alt="No Preview" fill className="object-contain max-sm:hidden object-right"/>
-        <div className=" md:py-20 grid xl:grid-cols-4 md:gap-16 gap-5 mx-auto max-w-7xl relative z-20 grid-cols-1 p-3 md:p-0 ">
+        <Image src='/Assests/Images/HomeImage/22.png' alt="No Preview" fill className="object-contain max-sm:hidden object-left-bottom"/>
+        <Image src='/Assests/Images/HomeImage/23.png' alt="No Preview" fill className="object-contain max-sm:hidden object-right"/>
+        <div className=" md:py-10 grid xl:grid-cols-4 md:gap-16 gap-5 mx-auto max-w-7xl relative z-20 grid-cols-1 p-4 md:p-0 items-start">
             <div className="p-2 grid md:gap-6 gap-3">
-                <div className="relative h-11 w-24">
+                <div className="relative h-11 w-24 ml-2">
                 <Image
                     src="/Assests/Images/HomeImage/01.png"
                     alt="No Preview"
                     fill
-                    className="object-contain"
+                    className="object-contain scale-125"
                 />
                 </div>
                 <p className="max-sm:hidden">
                 We manufacture 100 % effective ayurvedic medicine which is ISO, GMB and other Government certified license
                 </p>
             </div>
-            <div className="list-none grid md:gap-6 gap-3">
+            <div className="list-none grid md:gap-6 gap-3 items-start">
                 <h1 className={cn("text-3xl", forum.className)}>Quick Link</h1>
                 <div className="grid grid-cols-2 md:text-base text-sm">
-                    <div>
-                        <li>Home</li>
-                        <li>Haircare</li>
-                        <li>Skincare</li>
-                        <li>Men's</li>
+                    <div className="grid gap-4">
+                        <Link href={'/'} className="cursor-pointer hover:text-primaryMain">Home</Link>
+                        <Link href={'/hairCare'} className="cursor-pointer hover:text-primaryMain">Haircare</Link>
+                        <Link href={'/skinCare'} className="cursor-pointer hover:text-primaryMain">Skincare</Link>
+                        <Link href={'/mens'} className="cursor-pointer hover:text-primaryMain">Men's</Link>
                     </div>
-                    <div>
-                        <li>Health Care</li>
-                        <li>All Products</li>
-                        <li>About Us</li>
-                        <li>Contact Us</li>
+                    <div className="grid gap-4">
+                        <li className="cursor-pointer hover:text-primaryMain">Health Care</li>
+                        <Link href={'/AllProduct'} className="cursor-pointer hover:text-primaryMain">All Products</Link>
+                        <Link href={'/AboutUs'} className="cursor-pointer hover:text-primaryMain">About Us</Link>
+                        <Link href={'/ContactUs'} className="cursor-pointer hover:text-primaryMain">Contact Us</Link>
                     </div>
                 </div>
             </div>
             <div className="list-none grid md:gap-6 gap-3 md:text-base text-sm">
-                <h1 className={cn("text-3xl", forum.className)}></h1>
-                <div>
+                <h1 className={cn("text-3xl", forum.className)}>Help & Guide</h1>
+                <div className="grid gap-4">
                     <li>Term and Condition</li>
                     <li>Privacy Policy</li>
                     <li>Shipping and Delivery</li>
@@ -53,18 +54,18 @@ export default function Footer() {
                 <h1 className={cn("text-3xl", forum.className)}>Reservation</h1>
                 <div className="flex items-center md:gap-6 gap-2">
                     <div className=" border-[1px] rounded-full border-black p-2"><Icons.call /></div>
-                    <div className="grid">
-                        <h4 className="text-sm md:text-[1rem]">+91 89890 91645</h4>
-                        <h4 className="text-sm md:text-[1rem]">+91 94256 62832</h4>
+                    <div className="grid gap-2">
+                        <Link href={'tel:+91 89890 91645'} className="text-sm md:text-[1rem] hover:text-primaryMain">+91 89890 91645</Link>
+                        <Link href={'tel:+91 94256 62832'} className="text-sm md:text-[1rem] hover:text-primaryMain">+91 94256 62832</Link>
                     </div>
                 </div>
                 <div className="flex items-center md:gap-6 gap-2">
                     <div className=" border-[1px] rounded-full border-black p-2"><Icons.message /></div>
-                    <div className="text-sm md:text-[1rem]">pachmarhiayurveda@gmail.com</div>
+                    <Link href={'mailto:pachmarhiayurveda@gmail.com'} className="text-sm md:text-[1rem] hover:text-primaryMain">pachmarhiayurveda@gmail.com</Link>
                 </div>
                 <div className="flex items-center md:gap-6 gap-2">
                     <div className=" border-[1px] rounded-full border-black p-2"><Icons.location /></div>
-                    <span  className="text-sm md:text-[1rem]">Arvind Mark, Main Market, Madhya Pradesh</span>
+                    <span  className="text-sm md:text-[1rem] hover:text-primaryMain cursor-pointer">Arvind Mark, Main Market, Madhya Pradesh</span>
                 </div>
             </div>
         </div>
--- src/components/navbar2/page.tsx
+++ src/components/navbar2/page.tsx
@@ -1,65 +1,57 @@
+"use client"
+
+import { cn } from "@/lib/utils";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 export default function Navbar2() {
+  const pathname = usePathname();
+  const arr = [
+    {
+      name : "Home",
+      page : '/'
+    },
+    {
+      name : "Haircare",
+      page : '/hairCare'
+    },
+    {
+      name : "Skincare",
+      page : '/skinCare'
+    },
+    {
+      name : "Men's",
+      page : '/mens'
+    },
+    {
+      name : "All Products",
+      page : '/AllProduct'
+    },
+    {
+      name : "About Us",
+      page : '/AboutUs'
+    },
+    {
+      name : "Contact Us",
+      page : '/ContactUs'
+    },
+    
+  ]
   return (
-    <div className=" border-t-[1px] border-[#00AB55] bg-white sticky top-0 z-40">
-      <div className="list-none md:flex hidden justify-between text-xl py-7 font-medium text-[#000000] mx-auto max-w-7xl h-full ">
-        <Link href={"/"}>
-          <li className="hover:text-[#00AB55] cursor-pointer group">
-            <span className="group-hover:border-b-2 group-hover:border-[#00AB55] pb-2">
-              Home
-            </span>
-          </li>
-        </Link>
-
-        <Link href={"/hairCare"}>
-          <li className="hover:text-[#00AB55] cursor-pointer group">
-            <span className="group-hover:border-b-2 group-hover:border-[#00AB55] pb-2">
-              Haircare
-            </span>
-          </li>
-        </Link>
-
-        <Link href={"/skinCare"}>
-          <li className="hover:text-[#00AB55] cursor-pointer group">
-            <span className="group-hover:border-b-2 group-hover:border-[#00AB55] pb-2">
-              Skincare
-            </span>
-          </li>
-        </Link>
-
-        <Link href={"/mens"}>
-          <li className="hover:text-[#00AB55] cursor-pointer group">
-            <span className="group-hover:border-b-2 group-hover:border-[#00AB55] pb-2">
-              Men's
-            </span>
-          </li>
-        </Link>
-
-        <Link href={"/AllProduct"}>
-          <li className="hover:text-[#00AB55] cursor-pointer group">
-            <span className="group-hover:border-b-2 group-hover:border-[#00AB55] pb-2">
-              All Products
-            </span>
-          </li>
-        </Link>
-
-        <Link href={"/AboutUs"}>
-          <li className="hover:text-[#00AB55] cursor-pointer group">
-            <span className="group-hover:border-b-2 group-hover:border-[#00AB55] pb-2">
-              About Us
-            </span>
-          </li>
-        </Link>
-
-        <Link href={"/ContactUs"}>
-        <li className="hover:text-[#00AB55] cursor-pointer group">
-          <span className="group-hover:border-b-2 group-hover:border-[#00AB55] pb-2">
-            Contact Us
-          </span>
-        </li>
-        </Link>
+    <div className=" border-t-[1px] border-[#00AB55] bg-white sticky top-0 z-40 shadow-md xl:px-0 px-6">
+      <div className="list-none md:flex hidden justify-between text-xl py-7 font-medium text-[#000000] mx-auto max-w-7xl h-full">
+        {
+          arr?.map((ele,i)=>(
+            <Link href={ele?.page}>
+              <li className={cn(`hover:text-[#00AB55] cursor-pointer group`, pathname === ele?.page ? "text-[#00AB55]":"")}>
+                <span className={cn("group-hover:border-b-2 group-hover:border-[#00AB55] pb-2" , pathname === ele?.page ? "border-b-2 border-[#00AB55]":"")}>
+                  {ele?.name}
+                </span>
+              </li>
+            </Link>
+          ))
+        }
       </div>
     </div>
   );
