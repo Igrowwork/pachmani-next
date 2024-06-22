7dce41d0e941fb27bc23552bf76d3b3f7e95ce24
Aniket-Patidar <167737531+aniketpatidar123@users.noreply.github.com>
2024-06-22 10:28:22 +0530

    update

--- src/lib/types/order.ts
+++ src/lib/types/order.ts
@@ -1,28 +1,16 @@
-<<<<<<< Updated upstream
 import { Address } from "./address";
+import { IUser } from "./user";
 
 export interface IOrderItem {
   product: String;
   variant: String; 
   quantity: number;
 }
-export interface IOrder extends Document {
-  items: IOrderItem[];
-  shippingAddress: Address;
-=======
-import { IUser } from "./user";
-
-export interface IOrderItem {
-  product: String;
-  variant: String; // Add variant field
-  quantity: number;
-}
 
 export interface IOrder extends Document {
   user: IUser[];
   items: IOrderItem[];
-  shippingAddress: string;
->>>>>>> Stashed changes
+  shippingAddress: Address;
   paymentMethod: string;
   totalPrice: number;
   status: "pending" | "processing" | "shipped" | "delivered";
