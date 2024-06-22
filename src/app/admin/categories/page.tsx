e3182b53c20180daa12a27690dcb269f6e40cf91
Aniket <aniketkalawat88@gmail.com>
2024-06-22 18:59:27 +0530

    cms-admin-category-addproduct-orders-page-complete

---
+++ src/app/admin/categories/page.tsx
@@ -0,0 +1,52 @@
+import Image from 'next/image'
+import React from 'react'
+
+export default function Categories() {
+    const arr = [
+        {
+            name:'Haircare',
+            product:'11',
+            img:'/Assests/Images/HomeImage/05.png'
+        },
+        {
+            name:'Skincare',
+            product:'05',
+            img:'/Assests/Images/HomeImage/Skincare.png'
+        },
+        {
+            name:'Healthcare',
+            product:'25',
+            img:'/Assests/Images/HomeImage/07.png'
+        },
+        {
+            name:"Men's",
+            product:'10',
+            img:'/Assests/Images/HomeImage/men.png'
+        },
+        {
+            name:'Bestseller',
+            product:'30',
+            img:'/Assests/Images/HomeImage/09.png'
+        },
+    ]
+  return (
+    <div className='grid grid-cols-2 gap-6'>
+        {
+            arr?.map((ele,i)=> (
+                <div className='grid grid-cols-2 bg-white p-2 rounded-xl'>
+                    <div className='h-auto w-full relative rounded-xl overflow-hidden'>
+                        <Image src={ele?.img} alt='No Preview' fill className='object-cover' />
+                    </div>
+                    <div className='p-4'>
+                        <h1 className='text-xl font-medium'>{ele?.name}</h1>
+                        <div className='text-[#332F32] font-normal mt-2'>Totals Product - {ele?.product}</div>
+                        <button className='bg-primaryMain p-2 text-white rounded-sm w-full mt-4'>Add Product</button>
+                        <button className='text-primaryMain p-2 rounded-sm block w-full'>View all Product</button>
+                    </div>
+
+                </div>
+            ))
+        }
+    </div>
+  )
+}
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import { Icons } from '../icons'
+import AdminLeftGraph from './_components/adminLeftGraph'
+import { CiEdit } from "react-icons/ci";
+import { MdDeleteOutline } from "react-icons/md";
 
 export default function Admin() {
+  const arr = ["","","",""]
   return (
     <div>
       <div className='grid grid-cols-4 gap-6'>
@@ -48,9 +52,50 @@ export default function Admin() {
 
       </div>
 
-      <div>
-        
+      <div className='grid grid-cols-2 gap-6'>
+        <AdminLeftGraph />
+        <AdminLeftGraph />
       </div>
+      <div className='shadow-[0px_4px_16.3px_0px_rgba(0,0,0,0.08)]  bg-white rounded-xl'>
+        <div className='flex justify-between items-center sm:p-4 md:p-5 xl:p-6 font-medium'>
+          <div className="text-xl font-medium capitalize text-[#4C535F]">Recent Orders</div>
+          <div>More &#8594;</div>
+        </div>
+        <div className='text-[#8E95A9] grid grid-cols-8 justify-items-center bg-[#F8F8F8] w-full p-5'>
+          <div>Orders</div>
+          <div>Customers</div>
+          <div>Qty</div>
+          <div>Date</div>
+          <div>Revenue</div>
+          <div>Net Profit</div>
+          <div>Status</div>
+          <div>Action</div>
+        </div>
+        <div className=''>
+          {
+            arr?.map((ele,i) => (
+              <div key={i} className='text-[#555F7E] grid grid-cols-8 justify-items-center w-full p-5'>
+                <div>#32000200</div>
+                <div>Priscilla Warren</div>
+                <div>2</div>
+                <div>Jan 10, 2020</div>
+                <div>$253.82</div>
+                <div>$60.76</div>
+                <div>Completed</div>
+                <div className='flex text-xl gap-2 text-[#C8CAD8]'>
+                  <CiEdit />
+                  <MdDeleteOutline />
+                </div>
+              </div>
+
+            ))
+
+          }
+          
+        </div>
+
+      </div>
+      
       
     </div>
   )
