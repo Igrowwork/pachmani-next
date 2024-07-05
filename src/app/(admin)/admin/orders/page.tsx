308625267332fa7ee2273e264d40d68b516dd172
Aniket <aniketkalawat88@gmail.com>
2024-07-05 14:57:56 +0530

    stepper-admin

--- src/app/(admin)/admin/orders/page.tsx
+++ src/app/(admin)/admin/orders/page.tsx
@@ -47,36 +47,46 @@ export default function Orders() {
             <span>Orders</span>
             <span className='text-sm font-semibold'>More &#8594;</span>
         </div>
-        <div className='overflow-hidden'>
-            <div className='text-[#8E95A9] flex justify-center items-center bg-[#F8F8F8] w-fit p-5 rounded-t-lg mt-8 border-b border-b-[#E9EAF3] text-sm overflow-scroll'>
-            <div className='w-56'>Orders</div>
-            <div className='w-72'>Customers</div>
-            <div className='w-44'>Qty</div>
-            <div className='w-56'>Date</div>
-            <div className='w-56'>Revenue</div>
-            <div className='w-56'>Net Profit</div>
-            <div className='w-56'>Status</div>
-            <div className='w-56'>Action</div>
+        <div className=' overflow-scroll my-4'>
+            <div className='text-[#8E95A9] flex justify-center items-center bg-[#F8F8F8] w-fit p-5 rounded-t-lg mt-8 border-b border-b-[#E9EAF3] text-sm'>
+            <div className='w-14 text-center'>S.no.</div>
+            <div className='w-56 text-center'>Orders</div>
+            <div className='w-56 text-center'>Customers</div>
+            <div className='w-20'>Qty</div>
+            <div className='w-20 text-center'>Date</div>
+            <div className='w-56 text-center'>Revenue</div>
+            <div className='w-40'>Net Profit</div>
+            <div className='w-40'>Status</div>
+            <div className='w-40'>Action</div>
             </div>
-            <div className='w-full overflow-x-scroll  '>
+            <div className=''>
             {
-                isApi?.map((ele) => (
-                <div key={ele?.id} className='text-[#555F7E] flex justify-center items-center w-full p-6 border-b-2 border-b-[#E9EAF3] text-sm '>
-                    <div className='flex w-72'>{ele?._id}</div>
-                    <div className='flex items-center w-72'>
+                isApi?.map((ele , i) => (
+                <div key={ele?.id} className='text-[#555F7E] flex justify-center w-fit items-center p-6 border-b-2 border-b-[#E9EAF3] text-sm bg-white'>
+                    <div className='w-14 font-bold text-center pr-3'>{i+1}</div>
+                    <div className='flex w-56'>{ele?._id}</div>
+                    <div className='flex items-center w-56 gap-2 justify-center'>
                         <div className='relative h-10 w-10 rounded-full overflow-hidden'>
                             <Image src={'/Assests/Images/admin/01.png'} alt='No Preview' fill className='object-cover' />
                         </div>
                         <div className=''>Priscilla Warren</div>
                     </div>
-                    <div className='w-44'>2</div>
-                    <div className='w-56'>{ele?.createdAt}</div>
-                    <div className='w-56'> ₹ {ele.totalPrice}</div>
-                    <div className='w-56'>$60.76</div>
-                    <div className='w-56'>{ele?.paymentStatus}</div>
-                    <div className='flex text-xl gap-2 text-[#C8CAD8] w-56'>
+                    <div className='w-20'>2</div>
+                    <div className='w-20 text-center'>{new Date(ele?.createdAt).toLocaleDateString()}</div>
+                    <div className='w-56 text-center'> ₹ {ele.totalPrice}</div>
+                    <div className='w-40'>$60.76</div>
+                    <div className='w-40 text-sm'>
+                      <select>
+                        <option value={"completed"} className='text-primaryMain'>Completed</option>
+                        <option value={"Shipping"} className='text-[#2B2B4C]'>Shipping</option>
+                        <option value={"Pending"} className='text-[#FF392B]'>Pending</option>
+                        <option value={"Refund"} className='text-[#F2994A]'>Refund</option>
+                      </select>
+                    </div>
+                    <div className='flex text-xl gap-2 text-[#C8CAD8] w-40'>
                     <CiEdit />
                     <MdDeleteOutline />
+                    <span className=''>...</span>
                     </div>
                 </div>
 
